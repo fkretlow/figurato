9275657f6f41c98f4bb24416d321ccbb1fce2658
Florian Kretlow <floriankretlow@gmx.de>
2020-05-31 17:15:46 +0200

    change font name for FiguratoB

--- scripts/build.py
+++ scripts/build.py
@@ -1,21 +1,23 @@
 import fontforge
 
-def generateWithFeatures(font, fea, dest):
+def generateWithFeatures(font, fea, name, dest="./redist/"):
     # make sure no old lookups remain
     for l in font.gpos_lookups:
         f.removeLookup(l)
     for l in font.gsub_lookups:
         f.removeLookup(l)
     font.mergeFeature(fea)
-    font.generate(dest)
+    font.familyname = name
+    font.fontname = name
+    font.generate(dest + name + ".otf")
 
 
 if __name__ == "__main__":
     f = fontforge.open("./src/Figurato.sfd")
 
     print("generating Figurato.otf")
-    generateWithFeatures(f, "./src/figurato_T.fea", "./redist/Figurato.otf")
+    generateWithFeatures(f, "./src/figurato_T.fea", "Figurato")
     print("generating FiguratoB.otf")
-    generateWithFeatures(f, "./src/figurato_B.fea", "./redist/FiguratoB.otf")
+    generateWithFeatures(f, "./src/figurato_B.fea", "FiguratoB")
 
     f.close()
